9109221df4359ce0a9850e8eb717238e4cefbaab
James Southern <jms301@gmail.com>
2017-02-06 17:54:04 +0000

    tidy up colors

--- src/SensorChart.js
+++ src/SensorChart.js
@@ -21,74 +21,72 @@ export default class SensorChart extends React.Component {
 			datasets:[
 				{
 					yAxisID: "y-axis-0",
-					label: 'temperature', data: [],
+					label: 'Temperature', data: [],
 					fill: false,
 					type: 'line',
 
-					lineTension: 0.2,
-					backgroundColor: 'rgba(75,192,192,0.4)',
-					borderColor: 'rgba(75,192,192,1)',
+      		lineTension: 0.15,
+					backgroundColor: 'rgba(255, 153, 10, 0.2)',
+					borderColor: 'rgba(255, 153, 10, 1)',
+          borderWidth: 3,
 					borderCapStyle: 'butt',
-					borderDash: [],
+					borderDash: [10,5,5,5],
 					borderDashOffset: 0.0,
-					pointBorderColor: 'rgba(75,192,192,1)',
 					borderJoinStyle: 'miter',
+
+					pointRadius: 3,
+					pointBorderColor: 'rgba(255, 153, 10, 1)',
 					pointBackgroundColor: '#fff',
 					pointBorderWidth: 2,
+
 					pointHoverRadius: 5,
-					pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-					pointHoverBorderColor: 'rgba(220,220,220,1)',
-					pointHoverBorderWidth: 2,
-					pointRadius: 2,
+					pointHoverBackgroundColor: 'rgba(255, 153, 10, 1)',
+					pointHoverBorderColor: 'rgba(0, 0, 0, 1)',
+					pointHoverBorderWidth: 1,
 					pointHitRadius: 10,
+
+
+
 				},
 				{
 					yAxisID: "y-axis-0",
-					label: 'hum', data: [], fill: false,
+					label: 'Humidity', data: [], fill: false,
 					type: 'line',
-					lineTension: 0.2,
-					backgroundColor: 'rgba(75,255,192,0.4)',
-					borderColor: 'rgba(75,255,192,1)',
+					lineTension: 0.15,
+					backgroundColor: 'rgba(31, 211, 239, 0.2)',
+					borderColor: 'rgba(31, 211, 239, 1)',
+
+          //rgba(31, 211, 239, 1);
+          borderWidth: 3,
 					borderCapStyle: 'butt',
-					borderDash: [],
+					borderDash: [10,5],
 					borderDashOffset: 0.0,
-					pointBorderColor: 'rgba(75,255,192,1)',
 					borderJoinStyle: 'miter',
+
+					pointRadius: 3,
+					pointBorderColor: 'rgba(31, 211, 239, 1)',
 					pointBackgroundColor: '#fff',
 					pointBorderWidth: 2,
+
 					pointHoverRadius: 5,
-					pointHoverBackgroundColor: 'rgba(75,255,192,1)',
-					pointHoverBorderColor: 'rgba(220,220,220,1)',
-					pointHoverBorderWidth: 2,
-					pointRadius: 2,
+					pointHoverBackgroundColor: 'rgba(31, 211, 239, 1)',
+					pointHoverBorderColor: 'rgba(0, 0, 0, 1)',
+					pointHoverBorderWidth: 1,
 					pointHitRadius: 10,
 
 
 
 				},
 				{
-					fill: false,
 					yAxisID: "y-axis-1",
-					label: 'irrad', data: [],
+					label: 'Light', data: [],
 					type: 'bar',
-					//lineTension: 0.2,
-					backgroundColor: 'rgba(255,192,192,0.4)',
-					borderColor: 'rgba(255,192,192,1)',
-					borderCapStyle: 'butt',
-					borderDash: [],
-					borderDashOffset: 0.0,
-					pointBorderColor: 'rgba(255,192,192,1)',
-					borderJoinStyle: 'miter',
-					pointBackgroundColor: '#fff',
-					pointBorderWidth: 2,
-					pointHoverRadius: 5,
-					pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-					pointHoverBorderColor: 'rgba(220,220,220,1)',
-					pointHoverBorderWidth: 2,
-					pointRadius: 2,
-					pointHitRadius: 10,
-
-
+					backgroundColor: 'rgba(247, 230, 46, 0.7)',
+					borderColor: 'rgba(200, 190, 30, 0.7)',
+          borderWidth: 1,
+					hoverBackgroundColor: 'rgba(247, 230, 46, 1)',
+					hoverBorderColor: 'rgba(0,0,0,1)',
+					hoverBorderWidth: 1,
 				}
 			]
 		});
@@ -113,34 +111,6 @@ export default class SensorChart extends React.Component {
 				}]
 			}
 		};
-/*
-    const chartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-]
-
-    }; */
 
     return (
       <div>
